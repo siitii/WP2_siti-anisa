8ff46413d918f9584a29f6994da84bc2472af623
siitii <aynuraafifah@gmail.com>
2022-09-18 22:50:25 +0700

    latihan pertemuan 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"nama saya Siti Anisa
+            saya tinggal di Desa Kuala Dua Rasau Jaya
+            saya senang jalan-jalan melihat pemandangan
+            baru di tempat luar dari kota yang saya 
+            tempati saat ini";
+    }
+}
