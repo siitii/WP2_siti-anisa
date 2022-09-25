af9f7b0a403e8eb80819704b513677009d87b652
siitii <aynuraafifah@gmail.com>
2022-09-25 13:29:58 +0700

    mengubah

--- application/controllers/controller-latihan1.php
+++ application/controllers/controller-latihan1.php
@@ -6,12 +6,15 @@ class controller_latihan1 extends CI_Controller
         echo "Selamat Datang.. selamat belajar Web Programming";
         //$this->load->view('view-latihan1');
     }
- 
+
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('Model_latihan1');
-        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
-.$hasil;
+ 
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+ 
+        $this->load->view('view-latihan1', $data);
     }
 }
