f1d2f058c84117dcfac41f350060c5a7153f14ea
siitii <aynuraafifah@gmail.com>
2022-11-12 23:13:34 +0700

    tgs pertemuan 10

--- application/views/autentifikasi/blok.php
+++ application/views/autentifikasi/blok.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initialscale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <title>Akses diblock </title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/');?>vendor/fontawesomefree/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/');?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,30
     0i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -62,7 +62,7 @@
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" ariahidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
